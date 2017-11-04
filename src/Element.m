d8515668c415cff252d2bbd07931ba9764879bf4
Debjit Sarkar <debjits11@yahoo.com>
2017-11-04 02:17:13 -0400

    Create Element.m

---
+++ src/Element.m
@@ -0,0 +1,21 @@
+classdef  Element
+   properties
+      x %array [xmin xmax]
+      y %array [ymin ymax]
+      z %array [zmin zmax]
+      q %charge
+      m %mass
+      
+   end 
+   methods
+      function obj = methodName(obj,arg2,...)
+         ...
+      end
+   end
+   events
+      EventName
+   end
+   enumeration
+      EnumName
+   end
+   end
